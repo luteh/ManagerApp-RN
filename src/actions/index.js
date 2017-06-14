6785167ecd26c72824875038b5b92bcf6cd930d9
luteh <luthfanmaftuh@gmail.com>
2017-06-14 16:29:30 +0700

    Refactor the LOGIN_USER_SUCCESS

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@
  * Created by Luteh on 14/06/2017.
  */
 import firebase from 'firebase';
-import {EMAIL_CHANGED, PASSWORD_CHANGED} from './types';
+import {EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS} from './types';
 
 export const emailChanged = (text) => {
     return {
@@ -22,7 +22,7 @@ export const loginUser = ({email, password}) => {
     return (dispatch) => {
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(user => {
-                dispatch({type: 'LOGIN_USER_SUCCESS', payload: user});
+                dispatch({type: LOGIN_USER_SUCCESS, payload: user});
             });
     };
 };
