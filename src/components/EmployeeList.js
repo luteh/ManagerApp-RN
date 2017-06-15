57b665211fb047e9d11a64607fc418e600764f18
luteh <luthfanmaftuh@gmail.com>
2017-06-15 20:23:30 +0700

    Implemented lodash library for transform the objects to Arrays

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -3,15 +3,35 @@
  */
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import _ from 'lodash';
 import {employeesFetch} from '../actions';
-import {View, Text} from 'react-native';
+import {View, Text, ListView} from 'react-native';
 
 class EmployeeList extends Component {
-    componentWillMount(){
+    componentWillMount() {
         this.props.employeesFetch();
+
+        this.createDataSource(this.props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        // nextProps are the next set of props that this component
+        // will be rendered with
+        // this.props is still the old set of props
+
+        this.createDataSource(nextProps);
+    }
+
+    createDataSource({employees}) {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.dataSource = ds.cloneWithRows(employees);
     }
 
     render() {
+        console.log(this.props);
         return (
             <View>
                 <Text>EmployeeList</Text>
@@ -26,4 +46,12 @@ class EmployeeList extends Component {
     }
 }
 
-export default connect(null, {employeesFetch})(EmployeeList);
+const mapStateToProps = state => {
+    const employees = _.map(state.employees, (val, uid) => {
+        return {...val, uid};
+    });
+
+    return {employees};
+};
+
+export default connect(mapStateToProps, {employeesFetch})(EmployeeList);
