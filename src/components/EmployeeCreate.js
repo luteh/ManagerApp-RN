15ffcf7e2fa068b6d1b1941d525e3586c6e8e111
luteh <luthfanmaftuh@gmail.com>
2017-06-16 02:44:01 +0700

    Make each list item be able to click then redirect to employeeCreate and pass the props item

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -13,6 +13,7 @@ class EmployeeCreate extends Component {
     }
 
     render() {
+        console.log(this.props.employee);
         return (
             <Card>
                 <CardSection>
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,18 +2,27 @@
  * Created by Luteh on 15/06/2017.
  */
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {Text, View, TouchableWithoutFeedback} from 'react-native';
+import {Actions} from 'react-native-router-flux';
 import {CardSection} from './common';
 
 class ListItem extends Component {
+    onRowPress(){
+        Actions.employeeCreate({employee : this.props.employee});
+    }
+
     render() {
         const {name} = this.props.employee;
         return (
-            <CardSection>
-                <Text style={styles.titleStyle}>
-                    {name}
-                </Text>
-            </CardSection>
+        <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+            <View>
+                <CardSection>
+                    <Text style={styles.titleStyle}>
+                        {name}
+                    </Text>
+                </CardSection>
+            </View>
+        </TouchableWithoutFeedback>
         )
     }
 }
